e8fba6e0c7ebf6404635ab9a059f9e34945fb70e
Joe Carlson <joe.carlson@bestbuy.com>
2017-10-31 18:03:21 -0500

    hue working on rose hobart film/

--- src/client/components/VideoPlayer/VideoPlayer.js
+++ src/client/components/VideoPlayer/VideoPlayer.js
@@ -1,41 +1,92 @@
-import React from 'react';
-import {
-	Player,
-	ControlBar,
-	ReplayControl,
-	ForwardControl,
-	CurrentTimeDisplay,
-	TimeDivider,
-	LoadingSpinner,
-	PlaybackRateMenuButton,
-	VolumeMenuButton,
-} from 'video-react';
-import 'video-react/dist/video-react.css';
-import './VideoPlayer.scss';
-import poster from '../../assets/images/poster.png';
-import video from '../../assets/video/video.mp4';
+/*
+  eslint
+	jsx-a11y/media-has-caption: 0
+	react/no-string-refs: 0
+*/
 
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Shaders, GLSL, Node } from 'gl-react';
+import { Surface } from 'gl-react-dom';
+import raf from 'raf';
+import videoMP4 from '../../assets/video/video.mp4';
+
+export { videoMP4 };
+
+// We implement a component <Video> that is like <video>
+// but provides a onFrame hook so we can efficiently only render
+// if when it effectively changes.
+export class Video extends Component {
+	componentDidMount() {
+		const loop = () => {
+			this._raf = raf(loop);
+			const { video } = this.refs;
+			if (!video) return;
+			const { currentTime } = video;
+			// Optimization that only call onFrame if time changes
+			if (currentTime !== this.currentTime) {
+				this.currentTime = currentTime;
+				this.props.onFrame(currentTime);
+			}
+		};
+		this._raf = raf(loop);
+	}
+	componentWillUnmount() {
+		raf.cancel(this._raf);
+	}
+	render() {
+		const { onFrame, ...rest } = this.props;
+		return <video {...rest} ref="video" />;
+	}
+}
+
+Video.propTypes = {
+	onFrame: PropTypes.func.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+// Our example will simply split R G B channels of the video.
+const shaders = Shaders.create({
+	SplitColor: {
+		frag: GLSL`
+		precision highp float;
+		varying vec2 uv;
+		uniform sampler2D children;
+		void main () {
+		  float y = uv.y * 3.0;
+		  vec4 c = texture2D(children, vec2(uv.x, mod(y, 1.0)));
+		  gl_FragColor = vec4(
+		    c.r * step(2.0, y) * step(y, 3.0),
+		    c.g * step(1.0, y) * step(y, 2.0),
+		    c.b * step(0.0, y) * step(y, 1.0),
+		    1.0);
+		}`,
+	},
+// ^NB perf: in fragment shader paradigm, we want to avoid code branch (if / for)
+// and prefer use of built-in functions and just giving the GPU some computating.
+// step(a,b) is an alternative to do if(): returns 1.0 if a<b, 0.0 otherwise.
+});
+
+const SplitColor = ({ children }) => (
+	<Node shader={shaders.SplitColor} uniforms={{ children }} />
+);
+
+SplitColor.propTypes = {
+	children: PropTypes.node.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+// We now uses <Video> in our GL graph.
+// The texture we give to <SplitColor> is a (redraw)=><Video> function.
+// redraw is passed to Video onFrame event and Node gets redraw each video frame.
 export default () => {
 	return (
-		<div className="video_player">
-			<Player
-				poster={poster}
-			>
-				<source src={video} />
-				<LoadingSpinner />
-
-				<ControlBar>
-					<ReplayControl seconds={10} order={1.1} />
-					<ForwardControl seconds={30} order={1.2} />
-					<CurrentTimeDisplay order={4.1} />
-					<TimeDivider order={4.2} />
-					<PlaybackRateMenuButton
-						rates={[5, 2, 1, 0.5, 0.1]}
-						order={7.1}
-					/>
-					<VolumeMenuButton disabled />
-				</ControlBar>
-			</Player>
-		</div>
+		<Surface width={280} height={630} pixelRatio={1}>
+			<SplitColor>
+				{redraw => (
+					<Video onFrame={redraw} autoPlay loop>
+						<source type="video/mp4" src={videoMP4} />
+					</Video>
+				)}
+			</SplitColor>
+		</Surface>
 	);
 };
