4b42e0a1d09d3b81c79cc0e468be287691460604
Joe Carlson <joe.carlson@bestbuy.com>
2017-12-05 16:34:06 -0600

    Updated footer.

--- src/client/components/shared/Footer/Footer.js
+++ src/client/components/shared/Footer/Footer.js
@@ -3,14 +3,17 @@ import './Footer.scss';
 
 const Footer = () => (
 	<div className="footer purple darken-1">
-		<div className="container">
-			<h2>
-				<div className="footer_content page-footer transparent" >
-					<p>
+		<div className="footer-copyright">
+			<div className="container">
+				<div className="row">
+					<p className="grey-text text-lighten-4 left">
+						© 2018 Karlsson - For Study Purposes Only
+					</p>
+					<p className="grey-text text-lighten-4 right">
 						site by <a target="_blank" rel="noopener noreferrer" href="http://www.callmejoe.net/">joe </a> & <a target="_blank" rel="noopener noreferrer" href="http://www.callmejoe.net/">laurel</a>
 					</p>
 				</div>
-			</h2>
+			</div>
 		</div>
 	</div>
 );
